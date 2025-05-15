80da3f34ccdc2cbdd992c774e46398d57e6a5759
Afrian Suwandi <srian744@gmail.com>
2025-05-15 09:31:43 +0700

    new config

--- includes/config.php
+++ includes/config.php
@@ -4,10 +4,11 @@ $db = 'rianganteng';
 $user = 'root';
 $pass = ''; // default for Laragon
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+// Membuat koneksi
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
